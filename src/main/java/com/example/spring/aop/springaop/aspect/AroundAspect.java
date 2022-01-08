651c98d2d051d349a3025277f3debc5d88e79773
sancbansal <sancbansal@paypal.com>
2022-01-08 16:28:21 +0530

    @Around Aspect

---
+++ src/main/java/com/example/spring/aop/springaop/aspect/AroundAspect.java
@@ -0,0 +1,23 @@
+package com.example.spring.aop.springaop.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class AroundAspect {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Around(value = "execution(* com.example.spring.aop.springaop.business.*.*(..))")
+    public void around(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        joinPoint. proceed();
+        long timeTaken = System.currentTimeMillis() - startTime;
+        logger.info("time taken by {} is {}", joinPoint, timeTaken);
+    }
+}
